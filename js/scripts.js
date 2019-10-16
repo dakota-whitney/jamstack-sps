3e3c67bcbd12b3f4a9f2011354e1593c8aa3b8dc
Dakota Whitney <whitneydakota@gmail.com>
2019-10-15 20:51:43 -0400

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+document.addEventListener('click', clickHandlers);
+
+var nyt =
+  'https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=uQG4jhIEHKHKm0qMKGcTHqUgAolr1GM0';
+
+function clickHandlers() {
+  if (event.target.matches('button')) {
+    getData();
+  }
+}
+
+var addContent = function(data) {
+  // initialize an empty variable
+  var looped = '';
+
+  // use += in a for loop that uses the length of the results
+  for (i = 0; i < data.results.length; i++) {
+    looped += `
+        <div class="item">
+          <h3>${data.results[i].title}</h3>
+          <img src="${data.results[i].multimedia[1].url}" />
+          <p>${data.results[i].abstract}</p>
+        </div>
+        `;
+  }
+  document.querySelector('.content div').innerHTML = looped;
+};
+
+var getData = function() {
+  fetch(nyt)
+    .then(response => response.json())
+    .then(json => addContent(json));
+};
---
+++ posts/contact.md
@@ -0,0 +1,13 @@
+---
+layout: layout.html
+pageTitle: Contact Us
+navTitle: Contact
+tags: post
+pageClass: Contact
+---
+
+## Here's how:
+
+- 917 865 5517
+
+[Home](/)
